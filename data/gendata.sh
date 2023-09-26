10420c3be2ccd02438107a62f02d039b4d8fbedf
kerneyJ <gamerguy9801@gmail.com>
2023-09-26 13:13:15 -0500

    made gendata script make 1 million 1K files 1 thousand 1M files and 10 1G files

--- data/gendata.sh
+++ data/gendata.sh
@@ -2,3 +2,22 @@
 dd if=/dev/urandom of=./1K.data bs=1K count=1
 dd if=/dev/urandom of=./1M.data bs=1M count=1
 dd if=/dev/urandom of=./1G.data bs=1G count=1
+
+mkdir kilo
+mkdir mega
+mkdir giga
+
+for i in {1..1000000}
+do
+	dd if=/dev/urandom of=./kilo/$(printf "%07d" $i).data bs=1K count=1
+done
+
+for i in {1..1000}
+do
+	dd if=/dev/urandom of=./kilo/$(printf "%04d" $i).data bs=1M count=1
+done
+
+for i in {1..10}
+do
+	dd if=/dev/urandom of=./kilo/$(printf "%02d" $i).data bs=1G count=1
+done
