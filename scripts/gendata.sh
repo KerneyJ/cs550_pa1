41655b1fef510d11eb056dbeaaade589baaed656
kerneyJ <gamerguy9801@gmail.com>
2023-10-19 00:46:52 -0500

    removed Gigabyte from gendata

--- scripts/gendata.sh
+++ scripts/gendata.sh
@@ -11,7 +11,7 @@ dd if=/dev/urandom of=$2/data/1M.data bs=1M count=1
 python3 $2/scripts/cpmega.py $1 $2
 python3 $2/scripts/cpkilo.py $1 $2
 
-for i in {0..9}
-do
-	dd if=/dev/urandom of=$2/data/vm$1_1G_$(printf "%06d" $i).data bs=1G count=1
-done
+#for i in {0..9}
+#do
+#	dd if=/dev/urandom of=$2/data/vm$1_1G_$(printf "%06d" $i).data bs=1G count=1
+#done
