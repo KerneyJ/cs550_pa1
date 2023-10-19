aacd1b4d5d156afce95cac42f25d8ad834928f4a
Harrison <jmohr1@hawk.iit.edu>
2023-10-18 21:51:07 -0500

    added start of config-tun-test1.sh

---
+++ config/config-run-test1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Expects two arguments: network topology choice, and test choice.
+#Topology choice: 1: centralized, 2: star topology, 3: 2-D mesh topology
+#Test choice: 1: query latency, 2: query throughput, 3: Transfer throughput small, 4: Transfer throughput large
+#(See PA2 instructions for descriptions of the tests.)
+
+#If centralized, initiatialize centralized topology
+
+
+
+
+#else if star, initializae star topology
+
+
+
+
+#else if mesh, initialize mesh topology
+
+
+
+#If query latency: 
+
