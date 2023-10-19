1cb60164957170641c4b1c007be115f1f43cf58f
Ian Dougherty <ianedougherty01@gmail.com>
2023-10-19 00:59:49 -0500

    implemented decentralized file requests

--- src/decentralized_peer.cpp
+++ src/decentralized_peer.cpp
@@ -174,10 +174,94 @@ void DecentralizedPeer::search_index(conn_t client, msg_t request) {
 }
 
 void DecentralizedPeer::send_file(conn_t client, msg_t request) {
-	
+	msg_t response;
+	int msg_id;
+	std::string filename;
+
+	parse_message(&request, &msg_id, &filename);
+
+	printf("Received request for file: %s. (msg %d)\n", filename.c_str(), msg_id);
+
+	{
+		// Check if we have already seen this message
+		std::unique_lock<std::mutex> lock(query_map_lock);
+		if(received_queries.find(msg_id) != received_queries.end()) {
+			printf("Duplicate request (msg %d)\n", msg_id);
+			create_message(&response, DUP_REQUEST);
+			send_msg(response, client);
+			delete_msg(&response);
+			return;
+		}
+
+		received_queries.insert({ msg_id, { client.addr, FIXED_PORT } });
+	}
+
+	// Check if we have the file. if not, broadcast this message
+    if(file_set.find(filename) == file_set.end()) {
+		printf("File not here, broadcasting query to the homies...\n");
+        broadcast_query(client, &request, &response);
+
+		if(response.type == NULL_MSG) {
+			printf("Homies didn't have it...\n");
+			create_message(&response, DUP_REQUEST);
+			send_msg(response, client);
+			return;
+		}
+
+		delete_msg(&response);
+
+		printf("Forward the response\n");
+		char cfilename[] = {0};
+		memcpy(cfilename, filename.data(), filename.length());
+		send_msg(response, client);
+        return;
+    }
+
+	printf("We have the file!\n");
+
+	char cfilename[] = {0};
+	memcpy(cfilename, filename.data(), filename.length());
+
+	createfile_msg(&response, cfilename);
+    send_msg(response, client);
+	delete_msg(&response);
 }
 
 int DecentralizedPeer::request_file(std::string filename) {
+	int msg_id;
+	msg_t request, response;
+	conn_t peer;
+
+	printf("Searching for file...\n");
+
+	// The file is on this peer
+	if(file_set.find(filename) != file_set.end())
+		return 0;
+
+	printf("File not found locally...\n");
+
+	msg_id = get_message_id();
+	create_message(&request, msg_id, filename, REQUEST_FILE);
+
+	{
+		std::unique_lock<std::mutex> lock(query_map_lock);
+		received_queries.insert({ msg_id, server.get_conn_info() });
+	}
+
+	printf("Broadcasting query...\n");
+
+	broadcast_query(server.get_conn_info(), &request, &response);
+	delete_msg(&request);
+
+	if(response.type == STATUS_OK) {
+		printf("Got a valid response from the broadcast!\n");
+		delete_msg(&response);
+		return 0;
+	}
+
+	printf("Nobody had the file. :(\n");
+
+	delete_msg(&response);
 	return -1;
 }
 
