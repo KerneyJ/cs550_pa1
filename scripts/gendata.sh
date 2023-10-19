84d69635a2fe24426122063bf9106ccbcc304782
kerneyJ <gamerguy9801@gmail.com>
2023-10-19 00:46:22 -0500

    fixed gendata.sh

--- scripts/gendata.sh
+++ scripts/gendata.sh
@@ -8,8 +8,8 @@ fi
 dd if=/dev/urandom of=$2/data/1K.data bs=1K count=1
 dd if=/dev/urandom of=$2/data/1M.data bs=1M count=1
 
-python3 cpmega.py $1 $2
-python3 cpkilo.py $1 $2
+python3 $2/scripts/cpmega.py $1 $2
+python3 $2/scripts/cpkilo.py $1 $2
 
 for i in {0..9}
 do
