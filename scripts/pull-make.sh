5872eada901b1c8a9c961a198c3087b6d13a3eed
Chameleon Cloud User <cc@jaiaha.novalocal>
2023-10-26 17:43:14 +0000

    cleaning scripts

--- scripts/pull-make.sh
+++ scripts/pull-make.sh
@@ -3,3 +3,5 @@
 for i in {1..17}; do
         lxc exec "vm$i" -- bash -lc "cd /home/root/cs550_pa1 && git pull && make" &
 done
+
+wait
--- scripts/vm-setup.sh
+++ scripts/vm-setup.sh
@@ -8,7 +8,6 @@ lxc launch custom-image temp-container
 # lxc config set temp-container volatile.rootfs.size 10GB
 lxc config set temp-container limits.cpu 1
 lxc config set temp-container limits.memory 6GB
-lxc config device add temp-container root disk pool=temp-pool source=temp-vol path=/
 
 lxc exec temp-container -- apt autoremove -yy 
 lxc exec temp-container -- apt update -yy
@@ -30,7 +29,7 @@ num_containers=17
 # Create and start the containers using the custom image
 for i in $(seq 1 $num_containers); do
   container_name="vm$i"
-  lxc launch custom-image "$container_name" -c security.privileged=true --storage=temp-pool
+  lxc launch custom-image "$container_name" -c security.privileged=true
 done
 
 # Wait for the containers to be ready
@@ -41,10 +40,9 @@ $(pwd)/get-ips.sh
 # Control the containers
 for i in $(seq 1 $num_containers); do
   container_name="vm$i"
-  # Replace the following line with the command you want to run within each container example: lxc exec "$container_name" -- /bin/bash -l -c "date"
   lxc file push $(pwd)/peer-ips.csv $container_name/home/root/cs550_pa1/config/
   eval "lxc exec $container_name -- /home/root/cs550_pa1/scripts/gendata.sh $i /home/root/cs550_pa1" &
-  lxc exec $container_name -- bash -lc "echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCdNM8SfW+mdZlXGnvm1jV6uRyvIJH8o3wN91tfvdtvvYs2i9YDuQXWC3R6THRJJ4zH8XXt/PPyL0JxzlSHZO3u4screCyOqj4bjbub6W+AygXgIrC2eg9ErcfcKiWbzbajhwvXmXU3mk/ayOiDUGwJG65o/NgV5N01Ump7fvs8dNRI9mSBwmSH7sWd0dmf7SDi6hH8akrx5/PpnRWqZRD9mSHmssUttPwjmQyQt0eHeuY+jAfRC2+4gxE9JEP3Wlg2PWufpUavHhLwXNDgypzawDv4zs3XSZTj5tzuWBFhU2VUTxzxTcFUohhVFfHYn8WOFJIPbGzuKgynQiAizGMAa7iDXtcX879sWGI5IHp/OOUqsDzmzPe37/EzmPXphFsha2hiZ+lBpCurG5Ui4qum3aBPbXfQi4kMSOFgjMajVNsQ3KNOmOWMYTQbrKkJr8YJ/d4cFidscMyU5zx3fRWIujX7ZQfYsS3lBZC2qbxeVe/3Ar/UXaV0TYHDudbbg0s= cc@jaiaha-pa2' > /root/.ssh/authorized_keys && chmod 644 /root/.ssh/authorized_keys"
+  lxc exec $container_name -- bash -lc "echo ''ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCbon7ZO8PzmgB8zIMRxtius+suMFqQ0AKpVqgatew5FrzwCYdF204UzR/PM1bkLl23L9t0tzJ0XVYevjVLiggbfC15knHBhgOJXKxKhsFwc2K6u5UUazxJ4y6V9lL6cnho62Fx1HXQ0ZMPWPwAA3pKLEWZi47vOGYuxtKX8qgYNxkVI1WpNBoduYjDHZ/eq3w2dPrrtxBkr9h2hVQZuuxOFIC4b2Xx4jakP4NZHsOmQAc5UBuTQ5j9jBu/lkPiqFsjXCgR6233wULeEmqoMu7qp+r6lbx9SKVvuwY0hl5pMMlX0DaUyzn3cnspncKeKSZLC4AyTOpfBgRS+0ii02knoEkmu+2e317iVZ1/zk+sK1SQNIIC4rJhUk4nee+VvyUHUyMSl+yoTEthsRdp5WOLqaSUzBPX27Krm2FLCTtuVQyYN1Vt+lI+n9JqcZ5eVCpJl06AZkvE32XcofcDp9+cr47wblpwhRHIB8IHazUcIED9oDHMbuKv8wqirfIBdqk= cc@jaiaha > /root/.ssh/authorized_keys && chmod 644 /root/.ssh/authorized_keys"
   lxc exec vm$i -- bash -lc "cd /home/root/cs550_pa1 && echo '\$1 $i \$2 \$3' > ./scripts/start.sh && chmod +x ./scripts/start.sh"
 done
 
